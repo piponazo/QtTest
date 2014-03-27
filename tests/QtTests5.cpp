d82fc1f4e02dc40199c1a08384b519cb7e6674f7
Luis Díaz Más <piponazo@plagatux.es>
2014-03-27 10:22:27 +0100

    Added a new way to create a test with Qtest

---
+++ tests/QtTests5.cpp
@@ -0,0 +1,13 @@
+#include "QtTests4.h"
+#include "MyClass.h"
+#include <QtTest/QTest>
+
+void MyClassTest::testSlotCalled()
+{
+   MyClass c(this);
+   c.init(1);
+
+   QTest::qWait(5);
+
+   QVERIFY(c.wasOnTimerCalled() == true);
+}
--- tests/main_qtest.cpp
+++ tests/main_qtest.cpp
@@ -1,10 +1,13 @@
 #include <QtTest/QTest>
+#include <QtCore/QCoreApplication>
 #include "QtTests4.h"
 
 int main(int argc, char **argv)
 {
+   QCoreApplication app(argc, argv);
+
    MyClassTest test1;
-   int ret = QTest::qExec(&test1, argc, argv);
+   int ret = QTest::qExec(&test1);
 
    return ret;
 }
